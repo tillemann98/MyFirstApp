dcc93564667f7ef5f6369886ff6c9cd3686950fa
Benjamin Albert <Benjamin@Benjamins-Mac-Pro-93.local>
2021-02-06 02:17:48 +0100

    first commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Benjamin Albert on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
